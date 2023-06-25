8a2adedb841c3985932010311a50d9e7031e5487
50skydie <dieplsky@gmail.com>
2023-06-25 10:36:10 +0200

    log x time - fix, rm commented code

--- main.sh
+++ main.sh
@@ -13,12 +13,12 @@ while [[ $# -gt 0 ]]; do
            echo $2
            for ((i = 0; i <= $2; i++ ))
                do  
-               echo "log"$i.txt, $0, $(date)  # > log$i.txt
+               echo "log"$i.txt, $0, $(date) > log$i.txt
            done
        else
            for i in {1..100}
                do
-               echo "log"$i.txt, $0, $(date)  # > log$i.txt
+               echo "log"$i.txt, $0, $(date) > log$i.txt
             done
        fi
        shift
